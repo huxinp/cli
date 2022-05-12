3d5be6c7a9e2319da17fb72788f33330a43e0274
xinpeng.hu <xinpeng.hu@zhangmen.com>
2022-05-12 18:03:48 +0800

    fse copy

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const { Command } = require('commander');
 const download = require('download-git-repo');
 const fs = require('fs');
+const fse = require('fs-extra');
 const path = require('path');
 const handlebars = require('handlebars');
 const inquirer = require('inquirer');
@@ -11,28 +12,27 @@ const ora = require('ora');
 
 const program = new Command();
 
-const templates = {
-  'react': {
-    downloadUrl: '',
-    description: 'a模板'
-  },
-  'vue': {
-    downloadUrl: '',
-    description: 'b模板'
-  },
-  'electron': {
-    downloadUrl: '',
-    description: 'c模板'
-  },
-  'node': {
-    downloadUrl: '',
-    description: 'd模板'
-  }
-}
-
 const downloadTpl = (templateName, projectName, callback) => {
   // 模板项目保存在远程仓库
   const downloadFromGit = () => {
+    const templates = {
+      'react': {
+        downloadUrl: '',
+        description: 'a模板'
+      },
+      'vue': {
+        downloadUrl: '',
+        description: 'b模板'
+      },
+      'electron': {
+        downloadUrl: '',
+        description: 'c模板'
+      },
+      'node': {
+        downloadUrl: '',
+        description: 'd模板'
+      }
+    }
     // git 连接下载 
     const templateUrl = templates[templateName].downloadUrl; // 模板项目git地址
     if (!templateUrl) {
@@ -40,73 +40,37 @@ const downloadTpl = (templateName, projectName, callback) => {
     }
     download(templateUrl, projectName, { clone: true }, callback)
   }
-  const copy = (src, dest, callback) => {
-    if (!fs.existsSync(src)) {
-      throw new Error('模板不存在');
-      return
-    }
-    if (fs.existsSync(dest)) {
-      throw new Error('文件或文件夹已存在');
-      return
-    }
-    fs.mkdir(dest, (err) => {
-      if (err) {
-        throw new Error('创建文件夹失败');
-        return
-      }
-      const srcFile = fs.readdirSync(src);
-      for (const file of srcFile) {
-        const srcFile = path.resolve(src, file);
-        const destFile = path.resolve(dest, file);
-        if (fs.lstatSync(srcFile).isDirectory()) {
-          copy(srcFile, destFile, callback);
-        } else {
-          fs.copyFileSync(srcFile, destFile);
-        }
-      }
-    })
-  }
-  const copyFromLocal = () => {
-    const templatePath = path.resolve(__dirname, './templates', templateName);
+  const copyFromLocal = ([folder, templateName]) => {
+    const templatePath = path.resolve(__dirname, './templates', folder, templateName);
     const projectPath = projectName;
     if (!templatePath) {
       return callback(new Error('模板不存在'));
     }
-    try {
-      copy(templatePath, projectPath, callback);
-      callback();
-    } catch (err) {
-      callback(err);
+    if (fs.existsSync(projectName)) {
+      return callback(new Error('文件或文件夹已存在'));
     }
+    fse.copy(templatePath, projectPath, callback)
   }
-  return copyFromLocal();
+  return copyFromLocal(templateName.split(' '));
 }
 
 program
   .version('0.1.0', '-V, -v, --version', '脚手架工具版本信息')
-  
+
 program
-  .command('init <template> <project>')
+  .command('init <project>')
   .description('初始化项目模板')
-  .action(function (templateName, projectName) {
-    console.log(templateName, projectName)
-    const spinner = ora('正在下载模板...');
-    spinner.start();
-    downloadTpl(templateName, projectName, (err) => {
-      if (err) {
-        spinner.fail();
-        err.message && console.log(chalk.red(err.message))
-        return console.log(chalk.red('初始化模板失败'));
-      }
-      // 下载成功
-      spinner.succeed();
-      // 使用向导的方式采集用户输入的值
-      inquirer.prompt([
-        {
-          type: 'input',
-          name: 'name',
-          message: '请输入项目名称'
-        },
+  .action((projectName) => {
+    inquirer.prompt([{
+      type: 'list',
+      name: 'type',
+      message: '使用哪种技术栈',
+      choices: ['React', 'Vue', 'Electron', 'Typescript', 'Egg'],
+      filter(val) {
+        return val.toLowerCase();
+      },
+    }]).then(({ type }) => {
+      const questions = [
         {
           type: 'input',
           name: 'description',
@@ -116,27 +80,87 @@ program
           type: 'name',
           name: 'author',
           message: '请输入作者名称'
-        },
-      ]).then((answers) => {
-        console.log(answers);
-        // 使用模板引擎把用户输入的数据解析到 package.json 文件中
-        // 解析完毕， 把解析之后的结果重新写入package.json 文件中
-        console.log(process.argv)
-        const packagePath = `${projectName}/package.json`;
-        const packageContent = fs.readFileSync(packagePath, 'utf-8');
-        const packageResult = handlebars.compile(packageContent)(answers);
-        fs.writeFileSync(packagePath, packageResult);
-        console.log(chalk.green('初始化模板成功'));
+        }
+      ];
+      if (type !== 'electron') {
+        questions.unshift({
+          type: 'list',
+          message: '选择哪种打包方式',
+          name: 'build',
+          choices: ['Webpack', 'Rollup', 'Vite', 'Gulp'],
+          filter(val) {
+            return val.toLowerCase();
+          },
+        })
+      }
+      if (type === 'react') {
+        questions.unshift({
+          type: 'checkbox',
+          name: 'react',
+          message: '选择项目配置',
+          choices: ['router', 'redux', 'ts', 'eslint'],
+        });
+      }
+      if (type === 'vue') {
+        questions.unshift({
+          type: 'checkbox',
+          name: 'vue',
+          message: '选择项目配置',
+          choices: ['router', 'vuex', 'ts', 'eslint'],
+        });
+      }
+      inquirer.prompt(questions).then((answers) => {
+        const templateName = `${answers.build} ${[type, ...(answers[type] ? answers[type] : [])].join('-')}`;
+        const downloadSpinner = ora('正在下载模板...');
+        downloadSpinner.start();
+        downloadTpl(templateName, projectName, err => {
+          if (err) {
+            downloadSpinner.fail();
+            return console.log(chalk.redBright(err.message ? err.message : '下载模板失败'));
+          }
+          // 下载成功
+          downloadSpinner.succeed();
+          const initSpinner = ora('初始化模板...').start();
+          try {
+            const packagePath = path.resolve(__dirname, `${projectName}/package.json`);
+            const packageContent = fs.readFileSync(packagePath, 'utf-8');
+            const packageResult = handlebars.compile(packageContent)({ ...answers, name: projectName });
+            fs.writeFileSync(packagePath, packageResult);
+            initSpinner.succeed();
+          } catch(err) {
+            // console.log(err)
+            fse.remove(projectName); // 移除文件夹
+            initSpinner.fail();
+          }
+        })
       })
-    });
+    })
   })
   
 program
   .command('list')
   .description('查看所有的模板')
   .action(() => {
-    Object.entries(templates).forEach(([key, opt]) => {
-      console.log(key, opt.description)
+    const templateFolder = path.resolve(__dirname, './templates');
+    fse.readdir(templateFolder, (err, folders) => {
+      if (err) {
+        return console.log(chalk.redBright('出错了1'));
+      }
+      folders.forEach(folder => {
+        if (folder === 'electron') {
+          const files = fse.readdirSync(path.join(templateFolder, folder));
+          return files.length && console.log(folder);
+        }
+        fse.readdir(path.join(templateFolder, folder), (err, tpls) => {
+          if (err) {
+            return console.log(chalk.redBright('出错了'));
+          }
+          tpls.forEach(tpl => {
+            const files = fse.readdirSync(path.join(templateFolder, folder, tpl));
+            files.length && console.log(folder, tpl);
+          })
+        })
+      })
     })
   })
 
@@ -146,3 +170,4 @@ program.configureHelp({
 });
 
 program.parse(process.argv);
+
